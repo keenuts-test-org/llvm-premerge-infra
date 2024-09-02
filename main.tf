e84ebffd42b09fb87a948873aeff83812b4cd32e
Aiden Grossman <aidengrossman@google.com>
2024-09-02 21:47:54 +0000

    Switch to kubernetes container mode

--- main.tf
+++ main.tf
@@ -41,9 +41,10 @@ resource "google_container_node_pool" "llvm_premerge_linux_service" {
 }
 
 resource "google_container_node_pool" "llvm_premerge_linux" {
-  name     = "llvm-premerge-linux"
-  location = "us-central1-a"
-  cluster  = google_container_cluster.llvm_premerge.name
+  name               = "llvm-premerge-linux"
+  location           = "us-central1-a"
+  cluster            = google_container_cluster.llvm_premerge.name
+  initial_node_count = 1
 
   autoscaling {
     total_min_node_count = 1
@@ -63,25 +64,25 @@ resource "google_container_node_pool" "llvm_premerge_linux" {
   }
 }
 
-resource "google_container_node_pool" "llvm_premerge_windows" {
-  name     = "llvm-premerge-windows"
-  location = "us-central1-a"
-  cluster  = google_container_cluster.llvm_premerge.name
-  initial_node_count = 1
-
-  autoscaling {
-    total_min_node_count = 1
-    total_max_node_count = 2
-  }
-
-  node_config {
-    machine_type = "n1-highcpu-8"
-    labels = {
-      "premerge-platform" : "windows"
-    }
-    image_type = "WINDOWS_LTSC_CONTAINERD"
-  }
-}
+#resource "google_container_node_pool" "llvm_premerge_windows" {
+#  name     = "llvm-premerge-windows"
+#  location = "us-central1-a"
+#  cluster  = google_container_cluster.llvm_premerge.name
+#  initial_node_count = 1
+#
+#  autoscaling {
+#    total_min_node_count = 1
+#    total_max_node_count = 2
+#  }
+#
+#  node_config {
+#    machine_type = "n1-highcpu-8"
+#    labels = {
+#      "premerge-platform" : "windows"
+#    }
+#    image_type = "WINDOWS_LTSC_CONTAINERD"
+#  }
+#}
 
 provider "helm" {
   kubernetes {
@@ -130,24 +131,24 @@ resource "kubernetes_secret" "github_pat" {
   type = "Opaque"
 }
 
-resource "kubernetes_namespace" "llvm_premerge_windows_runners" {
-  metadata {
-    name = "llvm-premerge-windows-runners"
-  }
-}
+#resource "kubernetes_namespace" "llvm_premerge_windows_runners" {
+#  metadata {
+#    name = "llvm-premerge-windows-runners"
+#  }
+#}
 
-resource "kubernetes_secret" "windows_github_pat" {
-  metadata {
-    name      = "github-token"
-    namespace = "llvm-premerge-windows-runners"
-  }
-
-  data = {
-    "github_token" = data.google_secret_manager_secret_version.github_pat.secret_data
-  }
-
-  type = "Opaque"
-}
+#resource "kubernetes_secret" "windows_github_pat" {
+#  metadata {
+#    name      = "github-token"
+#    namespace = "llvm-premerge-windows-runners"
+#  }
+#
+#  data = {
+#    "github_token" = data.google_secret_manager_secret_version.github_pat.secret_data
+#  }
+#
+#  type = "Opaque"
+#}
 
 resource "helm_release" "github_actions_runner_controller" {
   name       = "llvm-premerge-linux"
@@ -175,16 +176,16 @@ resource "helm_release" "github_actions_runner_set" {
   depends_on = [kubernetes_namespace.llvm_premerge_linux_runners]
 }
 
-resource "helm_release" "github_actions_runner_set_windows" {
-  name       = "llvm-premerge-windows-runners"
-  namespace  = "llvm-premerge-windows-runners"
-  repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
-  version    = "0.9.3"
-  chart      = "gha-runner-scale-set"
-
-  values = [
-    "${file("windows_runner_values.yaml")}"
-  ]
-
-  depends_on = [kubernetes_namespace.llvm_premerge_windows_runners]
-}
+#resource "helm_release" "github_actions_runner_set_windows" {
+#  name       = "llvm-premerge-windows-runners"
+#  namespace  = "llvm-premerge-windows-runners"
+#  repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
+#  version    = "0.9.3"
+#  chart      = "gha-runner-scale-set"
+#
+#  values = [
+#    "${file("windows_runner_values.yaml")}"
+#  ]
+#
+#  depends_on = [kubernetes_namespace.llvm_premerge_windows_runners]
+#}
